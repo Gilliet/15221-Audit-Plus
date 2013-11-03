34d7c1c30c8517fb84e3b7a5f654ce45979a6e0b
Grant Della Silva <gdellasi@andrew.cmu.edu>
2013-11-02 20:46:58 -0400

    added script to output sample schedules as csv

---
+++ gen_sample_schedules.py
@@ -0,0 +1,23 @@
+# generate csv of schedules
+# $ python gen_sample_schedules.py <iters>
+
+
+from problem import *
+import sys
+
+if len(sys.argv) < 2:
+    print "Prints out <iter> schedules as comma seperated values"
+    print "   usage:  python gen_sample_schedues.py <iters>"
+    exit(1)
+
+a = scheduleProblem(initialState)
+
+for i in range(int(sys.argv[1])):
+    x = depth_first_tree_search(a).state[1]
+
+    for courseList in x.values():
+        for course in courseList:
+            temp = str(course) + ", "
+            sys.stdout.write(temp)
+    sys.stdout.write("\n")
+        
